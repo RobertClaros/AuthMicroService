2d2d9992337ccc6088174710cb877f816d1c51ce
RobertClaros <audioprobolivia@gmail.com>
2024-04-05 00:51:49 -0400

    Added github action pipeline

--- auth.test.js
+++ auth.test.js
@@ -1,13 +1,16 @@
-const { registerUser } = require('.//auht'); 
+/* global client */
+
+const { registerUser } = require('./auth');
+
 afterAll(async () => {
+  // Cerrar la conexión a MongoDB después de que todas las pruebas hayan finalizado
   await client.close();
 });
+
 describe('registerUser function', () => {
   it('should return error if username contains special characters', async () => {
-    const username = 'user@name'; 
-    const result = await registerUser(username, 'password');
-    
-    expect(result.success).toBe(false); 
-    expect(result.message).toMatch(/Invalid username/);
+    const result = await registerUser('user@name', 'password');
+    expect(result.success).toBe(false);
+    expect(result.message).toBe('Invalid username');
   });
 });
