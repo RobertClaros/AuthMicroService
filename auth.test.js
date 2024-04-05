87cbd2a6bbad6c8d6e299863a6520e5ab5d5416e
RobertClaros <audioprobolivia@gmail.com>
2024-04-05 00:48:07 -0400

    Added github action pipeline

--- auth.test.js
+++ auth.test.js
@@ -1,5 +1,7 @@
 const { registerUser } = require('.//auht'); 
-
+afterAll(async () => {
+  await client.close();
+});
 describe('registerUser function', () => {
   it('should return error if username contains special characters', async () => {
     const username = 'user@name'; 
